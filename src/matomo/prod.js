53cd7bb60c3b01d1bd9d701231f7f1343a5192bd
Adam Hlaváček <git@adamhlavacek.com>
2023-04-26 12:44:47 +0200

    patch(gymbk-manifest): plausible

--- src/matomo/prod.js
+++ src/matomo/prod.js
@@ -1,1 +1,7 @@
-console.debug('tracking disabled');
+(() => {
+  const plausible = document.createElement('script');
+  plausible.type = 'text/javascript';
+  plausible.dataset.domain = 'gymbkprg.adamhlavacek.com';
+  plausible.src = 'https://owl.adamhlavacek.com/js/script.js';
+  document.querySelector('head').appendChild(plausible);
+})();
